c9a168d3ce68a17d4603bebe92f644a5674010a7
bren2102 <brb.yg.21@gmail.com>
2020-05-22 18:03:46 -0500

    Add function to add marks on the board clicking on it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,3 @@
-const game = (() => {
-  // methods
-
-})();
-
 const displayController = (() => {
   let dom_board = document.getElementById('board');
   const renderBoard = (board) => {
@@ -11,7 +6,7 @@ const displayController = (() => {
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
         const cell_value = board[i][j];
-        rendered_board += ` <div class="cell">${cell_value}</div>`;
+        rendered_board += ` <div class="cell" onclick='gameBoard.applyMove(${i},${j})'>${cell_value}</div>`;
       }
     }
     dom_board.innerHTML = rendered_board;
@@ -23,18 +18,46 @@ const displayController = (() => {
 
 })();
 
-const gameBoard = (() => {
-  let board = [['X','O','X'],['','X','O'],['','X','O']];
+const gameBoard = ((dController) => {
+  let board = [['','',''],['','',''],['','','']];
+  let controller = dController;
+  let currentPlayer;
+  const setCurrentPlayer = (cPlayer) => {
+    currentPlayer = cPlayer;
+  }
   const getBoard = () => {
     return board;
   };
-  return {getBoard};
-})();
+  const applyMove = (indx, indy) => {
+    let symbol = currentPlayer.symbol;
+    if (board[indx][indy] === ''){
+      board[indx][indy] = symbol;
+      controller.renderBoard(board);
+    }
+    else{
+      alert('Spot taken');
+    }
+  };
+  return {setCurrentPlayer, getBoard, applyMove};
+})(displayController);
 
-const Player = () => {
-  let name;
-  let symbol;
-  return {};
+const Player = (name, symbol) => {
+  let name = name;
+  let symbol = symbol;
+  const getName = () => {
+    return name;
+  }
+  const getSymbol = () => {
+    return symbol;
+  }
+  return { getName, getSymbol };
 }
+
+const game = (() => {
+  let player1 = Player('Player 1', 'X');
+  let player2 = Player('Player 2', 'O');
+  let currentPlayer = player1;
+  gameBoard.setCurrentPlayer(currentPlayer);
+})();
 
 displayController.renderBoard(gameBoard.getBoard());
